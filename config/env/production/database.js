6a8e5166ee02da9ccf522ade25dc7d999430968a
Aliazbair <ali0995559052@gmail.com>
2021-12-14 19:06:03 +0300

    add

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,6 +1,6 @@
 const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
+module.exports = ({ env }) => { 
   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
   return {
